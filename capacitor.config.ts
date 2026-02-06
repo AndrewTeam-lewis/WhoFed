d54bd3a4be014305bafaa1293496b69dd880bf04
AndrewTeam-lewis <andrew@team-lewis.com>
2026-02-05 22:21:46 -0500

    android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.whofed.me',
   appName: 'WhoFed',
-  webDir: 'build'
+  webDir: 'build',
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"]
+    }
+  }
 };
 
 export default config;
