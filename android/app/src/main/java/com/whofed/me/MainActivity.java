83f72af0b444d912bbeac837986f28434286f5f9
AndrewTeam-lewis <andrew@team-lewis.com>
2026-02-16 10:58:47 -0300

    android

--- android/app/src/main/java/com/whofed/me/MainActivity.java
+++ android/app/src/main/java/com/whofed/me/MainActivity.java
@@ -1,11 +1,13 @@
 import android.os.Bundle;
-import androidx.activity.EdgeToEdge;
+// import androidx.activity.EdgeToEdge; // Removing to fix crash
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        EdgeToEdge.enable(this);
+        // EdgeToEdge.enable(this); // Removing
         super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
     }
 }
