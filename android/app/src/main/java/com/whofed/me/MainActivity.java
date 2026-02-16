7d16e243077184056b3f41099a3994921a66db63
AndrewTeam-lewis <andrew@team-lewis.com>
2026-02-16 12:12:01 -0300

    android

--- android/app/src/main/java/com/whofed/me/MainActivity.java
+++ android/app/src/main/java/com/whofed/me/MainActivity.java
@@ -9,12 +9,16 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
+import com.capacitorjs.plugins.statusbar.StatusBarPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
+
+        // Register StatusBar plugin (needed for header to work)
+        registerPlugin(StatusBarPlugin.class);
 
         // Force light mode system bars
         Window window = getWindow();
@@ -34,5 +38,8 @@ public class MainActivity extends BridgeActivity {
             WindowCompat.getInsetsController(window, decorView);
         windowInsetsController.setAppearanceLightStatusBars(true);
         windowInsetsController.setAppearanceLightNavigationBars(true);
+
+        // Force show the navigation bar
+        windowInsetsController.show(androidx.core.view.WindowInsetsCompat.Type.navigationBars());
     }
 }
