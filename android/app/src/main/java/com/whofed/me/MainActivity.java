acec2cf16ee96a99f84b4ece7214fc5d5fae52a5
AndrewTeam-lewis <andrew@team-lewis.com>
2026-02-16 10:52:30 -0300

    android

--- android/app/src/main/java/com/whofed/me/MainActivity.java
+++ android/app/src/main/java/com/whofed/me/MainActivity.java
@@ -1,5 +1,11 @@
-package com.whofed.me;
-
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
+        super.onCreate(savedInstanceState);
+    }
+}
